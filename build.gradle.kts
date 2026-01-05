b6c642d69130da7b2f4ea0e9d166f28cc83eee04
koflox <koflox.wmail@gmail.com>
2026-01-05 17:22:33 +0900

    detekt formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,24 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.detekt)
+}
+
+dependencies {
+    detektPlugins(libs.detekt)
+}
+
+tasks.register("detektRun", io.gitlab.arturbosch.detekt.Detekt::class) {
+    description = "Runs detekt analysis over whole code base"
+    parallel = true
+    autoCorrect = true
+    config.setFrom(files("$rootDir/.lint/detekt/detekt-config.yml"))
+    setSource(files(projectDir))
+    include("**/*.kt", "**/*.kts")
+    exclude("**/build/**", "**/resources/**")
+    reports {
+        xml.required.set(true)
+        html.required.set(false)
+        txt.required.set(false)
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "CyclingAssistant"
 include(":app")
- 
