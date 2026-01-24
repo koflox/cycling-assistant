06eba49ff90cd352466a96c7857ba6bf6f7b6e08
koflox <koflox.wmail@gmail.com>
2026-01-24 15:14:07 +0900

    fix camera movement before

--- feature/destinations/src/main/java/com/koflox/destinations/presentation/destinations/components/GoogleMapView.kt
+++ feature/destinations/src/main/java/com/koflox/destinations/presentation/destinations/components/GoogleMapView.kt
@@ -9,7 +9,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -61,17 +64,19 @@ internal fun GoogleMapView(
     val isDarkTheme = LocalDarkTheme.current
     val cameraPositionState = rememberCameraPositionState()
     val context = LocalContext.current
-
-    LaunchedEffect(selectedDestination) {
-        selectedDestination?.let { destination ->
-            moveCameraToLocation(
-                cameraPositionState = cameraPositionState,
-                location = destination.location,
-            )
+    var isMapLoaded by remember { mutableStateOf(false) }
+    LaunchedEffect(selectedDestination, isMapLoaded) {
+        if (isMapLoaded) {
+            selectedDestination?.let { destination ->
+                moveCameraToLocation(
+                    cameraPositionState = cameraPositionState,
+                    location = destination.location,
+                )
+            }
         }
     }
-    LaunchedEffect(cameraFocusLocation) {
-        if (cameraFocusLocation != null && selectedDestination == null) {
+    LaunchedEffect(cameraFocusLocation, isMapLoaded) {
+        if (isMapLoaded && cameraFocusLocation != null && selectedDestination == null) {
             moveCameraToLocation(cameraPositionState, cameraFocusLocation)
         }
     }
@@ -85,6 +90,7 @@ internal fun GoogleMapView(
         isSessionActive = isSessionActive,
         onSelectedMarkerInfoClick = onSelectedMarkerInfoClick,
         isDarkTheme = isDarkTheme,
+        onMapLoaded = { isMapLoaded = true },
     )
 }
 
@@ -99,6 +105,7 @@ private fun Map(
     isSessionActive: Boolean,
     onSelectedMarkerInfoClick: () -> Unit,
     isDarkTheme: Boolean,
+    onMapLoaded: () -> Unit,
 ) {
     val density = context.resources.displayMetrics.density
     val userLocationBitmap = remember(density) {
@@ -123,6 +130,7 @@ private fun Map(
         modifier = modifier,
         cameraPositionState = cameraPositionState,
         properties = mapProperties,
+        onMapLoaded = onMapLoaded,
     ) {
         userLocation?.let {
             val userLocationIcon = remember(userLocationBitmap) {
