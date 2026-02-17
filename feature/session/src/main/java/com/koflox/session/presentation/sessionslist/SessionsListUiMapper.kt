e4895e4f880d8bb07da2c9fd1d940989e7028d33
koflox <koflox.wmail@gmail.com>
2026-02-18 00:38:25 +0900

    fix: localization of numbers

--- feature/session/src/main/java/com/koflox/session/presentation/sessionslist/SessionsListUiMapper.kt
+++ feature/session/src/main/java/com/koflox/session/presentation/sessionslist/SessionsListUiMapper.kt
@@ -19,19 +19,19 @@ internal class SessionsListUiMapperImpl(
         private const val DATE_FORMAT_PATTERN = "MMM dd, yyyy HH:mm"
     }
 
-    override fun toUiModel(session: Session): SessionListItemUiModel {
-        val locale = localizedContextProvider.getLocalizedContext().resources.configuration.locales[0]
-        return SessionListItemUiModel(
-            id = session.id,
-            destinationName = session.destinationName,
-            dateFormatted = SimpleDateFormat(DATE_FORMAT_PATTERN, locale).format(Date(session.startTimeMs)),
-            distanceFormatted = String.format(Locale.getDefault(), "%.2f", session.traveledDistanceKm),
-            status = when (session.status) {
-                SessionStatus.RUNNING -> SessionListItemStatus.RUNNING
-                SessionStatus.PAUSED -> SessionListItemStatus.PAUSED
-                SessionStatus.COMPLETED -> SessionListItemStatus.COMPLETED
-            },
-            isShareButtonVisible = session.status == SessionStatus.COMPLETED,
-        )
-    }
+    private val appLocale: Locale
+        get() = localizedContextProvider.getLocalizedContext().resources.configuration.locales[0]
+
+    override fun toUiModel(session: Session): SessionListItemUiModel = SessionListItemUiModel(
+        id = session.id,
+        destinationName = session.destinationName,
+        dateFormatted = SimpleDateFormat(DATE_FORMAT_PATTERN, appLocale).format(Date(session.startTimeMs)),
+        distanceFormatted = String.format(appLocale, "%.2f", session.traveledDistanceKm),
+        status = when (session.status) {
+            SessionStatus.RUNNING -> SessionListItemStatus.RUNNING
+            SessionStatus.PAUSED -> SessionListItemStatus.PAUSED
+            SessionStatus.COMPLETED -> SessionListItemStatus.COMPLETED
+        },
+        isShareButtonVisible = session.status == SessionStatus.COMPLETED,
+    )
 }
