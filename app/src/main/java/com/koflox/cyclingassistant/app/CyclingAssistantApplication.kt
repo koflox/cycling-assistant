49476f0916816d6598246117752f275089393247
koflox <koflox.wmail@gmail.com>
2026-03-10 04:03:11 +0900

    performance: strict mode for debug builds

--- app/src/main/java/com/koflox/cyclingassistant/app/CyclingAssistantApplication.kt
+++ app/src/main/java/com/koflox/cyclingassistant/app/CyclingAssistantApplication.kt
@@ -1,6 +1,7 @@
 package com.koflox.cyclingassistant.app
 
 import android.app.Application
+import com.koflox.concurrent.enableStrictMode
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -8,6 +9,7 @@ import org.koin.core.context.startKoin
 class CyclingAssistantApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        enableStrictMode(this)
         startKoin {
             androidLogger()
             androidContext(this@CyclingAssistantApplication)
